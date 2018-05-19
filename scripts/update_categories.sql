3325cc2b86dc1029827d05f1e9613dd91cb47220
tomasPastore <tpastore@dc.uba.ar>
2018-05-18 21:06:03 -0300

    hola

--- scripts/update_categories.sql
+++ scripts/update_categories.sql
@@ -4,30 +4,26 @@ CREATE OR REPLACE VIEW categoriasActuales AS
          WHERE
          NOT t.bloqueada AND
          t.cliente_id = c.cliente_id AND
-         t.numero_de_tarjeta = cs.numero_de_tarjeta AND
-         cs.fecha > ALL (SELECT csi.fecha 
+         cs.fecha >= ALL (SELECT csi.fecha 
             FROM cambia_su csi, tarjeta ta
             WHERE
-                cs.fecha != csi.fecha AND 
-                ta.numero_de_tarjeta = csi.numero_de_tarjeta AND
-                ta.cliente_id = c.cliente_id);
-
+                ta.numero_de_tarjeta = csi.numero_de_tarjeta);
+         
 CREATE OR REPLACE VIEW resumenConsumos AS
-SELECT c.cliente_id, AVG(co.importe) as promedio, SUM(co.importe) as total 
-FROM cliente c, pago p, factura f, consumo co, categoriasActuales ca
+SELECT ca.cliente_id, SUM(co.importe) as total 
+FROM pago p, factura f, consumo co, categoriasActuales ca 
 WHERE
-    p.cliente_id = c.cliente_id AND
-    c.cliente_id = ca.cliente_id AND
-    co.numero_de_factura = f.numero_de_factura AND
+    p.cliente_id = ca.cliente_id AND
     f.pago_id = p.pago_id AND
+    co.numero_de_factura = f.numero_de_factura AND
     p.fecha > ca.fecha
-GROUP BY c.cliente_id;
+GROUP BY ca.cliente_id; 
 
 delimiter //
 
 DROP FUNCTION IF EXISTS tpbases.new_category//
 CREATE FUNCTION new_category(actual integer unsigned,
-     promedio decimal(10,2), total decimal(10,2),
+	 total decimal(10,2),
      ultimo_cambio date, fecha_actual date)
 RETURNS INTEGER UNSIGNED
 DETERMINISTIC
@@ -35,28 +31,33 @@ BEGIN
     DECLARE actual_monto_subida decimal(10,2);
     DECLARE actual_monto_permanencia decimal(10,2);
     DECLARE new INTEGER UNSIGNED;
+    DECLARE promedio decimal(10, 2);
 
-    SELECT c.monto_subida, c.monto_permanencia
-    INTO actual_monto_subida, actual_monto_permanencia
+    SELECT c.monto_subida, c.monto_permanencia, total / 12.0
+    INTO actual_monto_subida, actual_monto_permanencia, promedio
     FROM categoria c
     WHERE c.categoria_id = actual;
     
-    IF total > actual_monto_subida THEN 
-        SELECT categoria_id INTO new 
-        FROM categoria WHERE categoria.monto_subida <= total
-        ORDER BY categoria.monto_subida DESC
-        LIMIT 1;
-        IF new = actual THEN
-            RETURN (NULL);
-        END IF;
-    ELSEIF fecha_actual > ultimo_cambio + interval '365' day THEN
-        IF promedio < actual_monto_permanencia THEN
+
+    IF fecha_actual >= ultimo_cambio + interval '365' day THEN
+        IF actual_monto_permanencia > promedio THEN
             SELECT categoria_id INTO new
             FROM categoria
             WHERE categoria.monto_permanencia <= promedio
             ORDER BY categoria.monto_permanencia DESC 
             LIMIT 1;
         ELSE RETURN (actual);
+        END IF;
+    
+    ELSE    
+        SELECT categoria_id INTO new 
+        FROM categoria 
+        WHERE categoria.monto_subida <= total AND 
+        categoria.monto_subida >= actual_monto_subida
+        ORDER BY categoria.monto_subida DESC
+        LIMIT 1;
+        IF new = actual THEN
+            RETURN (NULL);
         END IF;
     END IF;
     RETURN (new);
@@ -68,12 +69,12 @@ BEGIN
         INSERT INTO cambia_su
         SELECT  fecha_actual,
                 numero_de_tarjeta as tarjeta,
-                new_category(categoria_id, promedio, total, fecha, fecha_actual) as new
-        FROM
-            (select c.cliente_id, c.categoria_id, c.fecha, IFNULL(r.promedio, 0) as promedio,
-                    IFNULL(r.total, 0) as total, c.numero_de_tarjeta from 
-            categoriasActuales c
-            LEFT OUTER JOIN resumenConsumos r ON c.cliente_id = r.cliente_id) t 
+                new_category(categoria_id, total, fecha, fecha_actual) as new 
+        FROM 
+            (select c.categoria_id, c.fecha, IFNULL(r.total, 0) as total, c.numero_de_tarjeta 
+            from categoriasActuales c LEFT JOIN resumenConsumos r ON c.cliente_id = r.cliente_id) t 
+        
+        GROUP BY fecha_actual, numero_de_tarjeta, new
         HAVING new IS NOT NULL;
 END//
 
