e09f4718ac9f410cf735f4f960c00681b7d9c729
Pato Lopez Valiente <patriciolopezvaliente@gmail.com>
2018-07-08 23:45:22 -0300

    ej3

--- tp2/queryd.py
+++ tp2/queryd.py
@@ -18,11 +18,42 @@ def queryd(id_parque):
         ).\
         pluck('id', 'nombre', 'total_consumos')
 
+def topFiveEventos():
+    return r.table('pago').\
+        concat_map(
+            lambda doc: doc['facturas'].\
+                concat_map(lambda fact: fact['consumos'].\
+                    concat_map(lambda cons: [
+                        {
+                        'id_medio': cons['medio_entretenimiento']['id'], 
+                        'tipo': cons['medio_entretenimiento']['tipo']['nombre'],
+                        'nombre': cons['medio_entretenimiento']['nombre'],
+                        'importe': cons['importe']
+                        }
+                        ]))).\
+        filter( r.row['tipo'] == ('EVENTO')).group('id_medio', 'nombre').\
+        sum('importe').\
+        ungroup().\
+        map(
+            lambda val:
+            {
+            'id': val['group'][0],
+            'evento': val['group'][1],
+            'importe': val['reduction']
+            }).\
+        order_by(r.desc('importe')).\
+        slice(0,5)
+
+
 if __name__ == '__main__':
-    if(len(sys.argv) != 2):
-        print(u"Ingresar el id del parque")
-    else:
-        conn = r.connect()
-        conn.use('tp2')
-        for value in queryd(int(sys.argv[1])).run(conn):
-            print(value)
+    cliente_id = 2
+    if(len(sys.argv) == 2):
+        parque_id = int(sys.argv[1])
+    conn = r.connect()
+    conn.use('tp2')
+    print('TOP FIVE EVENTOS EJ3')
+    for value in topFiveEventos().run(conn):
+        print(value)
+    print('ATRACCIONES POR CLIENTE', cliente_id, 'EJ1')
+    for value in queryd(cliente_id).run(conn):
+        print(value)
