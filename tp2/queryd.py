e85b4404627d66105c70520553f93baa572c7f58
Pato Lopez Valiente <patriciolopezvaliente@gmail.com>
2018-07-09 00:18:32 -0300

    ej4 y unas fallas de Jschema

--- tp2/queryd.py
+++ tp2/queryd.py
@@ -44,16 +44,37 @@ def topFiveEventos():
         order_by(r.desc('importe')).\
         slice(0,5)
 
+def atrPorClienteConRepeticiones(cliente_id): 
+    return r.table('tarjeta').\
+        filter( r.row['id_cliente'] == cliente_id).\
+        concat_map(
+            lambda doc: doc['consumos'].\
+                concat_map(lambda cons: [
+                    {
+                    'id': cons['medio_entretenimiento']['id'],
+                    'nombre': cons['medio_entretenimiento']['nombre'],
+                    'tipo': cons['medio_entretenimiento']['tipo']['nombre']
+                    }
+                    ])).\
+        filter( r.row['tipo'] == ('ATRACCION')).\
+        pluck('id', 'nombre')
+
+def atrPorCliente(cliente_id):
+    return atrPorClienteConRepeticiones(cliente_id).distinct()
 
 if __name__ == '__main__':
-    cliente_id = 2
+    parque_id = 2
     if(len(sys.argv) == 2):
         parque_id = int(sys.argv[1])
     conn = r.connect()
     conn.use('tp2')
-    print('TOP FIVE EVENTOS EJ3')
+    print('TOP FIVE EVENTOS - EJ3')
     for value in topFiveEventos().run(conn):
         print(value)
-    print('ATRACCIONES POR CLIENTE', cliente_id, 'EJ1')
-    for value in queryd(cliente_id).run(conn):
+    print('ATRACCIONES POR PARQUE:', parque_id, '- EJ4')
+    for value in queryd(parque_id).run(conn):
         print(value)
+    print('ATRACCIONES POR CLIENTE:', parque_id, '- EJ1')
+    for value in atrPorCliente(parque_id).run(conn):
+        print(value)
+ 
