b62d1e29288d89a0c84b20d680f767bad44f7a43
fbalboa <fernando.balboa@mercadolibre.com>
2018-07-09 17:22:46 -0300

    Mejoras a experimento + graficador

--- tp2/experimento.py
+++ tp2/experimento.py
@@ -1,6 +1,9 @@
 import rethinkdb as r
 import json
 import numpy as np
+import seaborn as sns
+import pandas as pd
+
 conn = r.connect()
 
 r.db_list().contains('experimento')\
@@ -24,25 +27,37 @@ r.table_list().contains('consumo')\
     )
   ).run(conn)
 
-r.table_create('consumo', shards=3, primary_key='id').run(conn)
+CANT_SHARDS = 3
+
+r.table_create('consumo', shards=CANT_SHARDS, primary_key='id').run(conn)
 
 f = open('consumos.json')
 consumos = json.load(f)
 f.close()
 
-n = 500
-particiones = [consumos[i:i+n] for i in range(0, len(consumos), n)]
+docs_por_medicion = 500
+particiones = [consumos[i : i + docs_por_medicion] for i in range(0, len(consumos), docs_por_medicion)]
 print(len(particiones))
-data = []
 
+data = {'cant_docs_total': [], 'cant_docs_shard': [], 'shard': []}
+
+docs_agregados = 0
 for i, p in enumerate(particiones):
     r.table('consumo').wait().run(conn)
     print('Insertando bloque ' + str(i+1))
     r.table('consumo').insert(p).run(conn)
+    docs_agregados += docs_por_medicion
     print('Rebalanceando')
     r.table('consumo').rebalance().run(conn)
     r.table('consumo').wait().run(conn)
-    data.append(r.table('consumo').info().run(conn)['doc_count_estimates'])
+    doc_count_estimates = r.table('consumo').info().run(conn)['doc_count_estimates']
+    for i in range(len(doc_count_estimates)):
+      data['cant_docs_total'] += [docs_agregados]
+      data["cant_docs_shard"] += [doc_count_estimates[i]]
+      data["shard"] += [i+1]
     print('')
 
-print(data)
+df = pd.DataFrame(data=data)
+df.to_csv("datos_exp.csv")
+
+
